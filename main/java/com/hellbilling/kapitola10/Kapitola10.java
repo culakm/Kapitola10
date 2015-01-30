6084e8a813aa8c35103c9d25735bf99f90dc0fd9
culakm <culak.martin@gmail.com>
2015-01-31 00:50:38 +0100

    pridane aktivity 2 a 3 ktore sa daju spustit cez radio a button

--- main/java/com/hellbilling/kapitola10/Kapitola10.java
+++ main/java/com/hellbilling/kapitola10/Kapitola10.java
@@ -1,14 +1,19 @@
 package com.hellbilling.kapitola10;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 
 public class Kapitola10 extends Activity
-        implements RadioGroup.OnCheckedChangeListener {
+        implements RadioGroup.OnCheckedChangeListener{
     RadioGroup orientation;
     RadioGroup gravity;
 
@@ -45,5 +50,34 @@ public class Kapitola10 extends Activity
                 gravity.setGravity(Gravity.RIGHT);
                 break;
         }
+    }
+
+    public void mcSetActivity(View view) {
+
+        // Is the button now checked?
+
+        //Intent intent = new Intent(this, activity3.class);
+        Toast.makeText(getApplicationContext(), " PAKO  "+ gravity.getId() + " vs. " + R.id.left, Toast.LENGTH_SHORT).show();
+        switch(gravity.getCheckedRadioButtonId()) {
+            case R.id.left:
+              //  if (checked)
+                Toast.makeText(getApplicationContext(), " activity 2 ", Toast.LENGTH_SHORT).show();
+                Intent intent2 = new Intent(this, activity2.class);
+                startActivity(intent2);
+                break;
+            case R.id.center:
+                //if (checked)
+                Toast.makeText(getApplicationContext(), " activity 3 ", Toast.LENGTH_SHORT).show();
+                Intent intent3 = new Intent(this, activity2.class);
+                startActivity(intent3);
+                break;
+            case R.id.right:
+                //if (checked)
+                //Intent intent = new Intent(this, activity3.class);
+                break;
+        }
+
+
+
     }
 }
